7508d52cfd71b63eb92b3b41678ac3b7cae63c3d
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2020-02-04 00:35:01 +0800

    Initial commit

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 3;
+
+    int currentSceneIndex;
+    
+    void Start()
+	{
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		if (currentSceneIndex == 0)
+		{
+            StartCoroutine(WaitAndLoad(timeToWait));
+		}
+    }
+
+    IEnumerator WaitAndLoad(int time)
+	{
+        yield return new WaitForSeconds(time);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+	{
+        SceneManager.LoadScene(currentSceneIndex + 1);
+	}
+
+    public void OnApplicationQuit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
