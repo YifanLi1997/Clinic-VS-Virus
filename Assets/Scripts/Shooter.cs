b7d52499e351252b881ece85c71ffad9e7cf1269
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2020-02-09 22:03:53 +0800

    Before dealing projectile colliding with attacker

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    [SerializeField] GameObject projectilePrefab;
+    [SerializeField] GameObject gunPosition;
+
+    public void Fire()
+    {
+        Instantiate(
+            projectilePrefab,
+            gunPosition.transform.position,
+            transform.rotation);
+    }
+}
