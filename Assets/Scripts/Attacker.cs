fa20f06513ccb693df9957212950dad0c0d48f18
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2020-02-09 13:40:13 +0800

    Scripted Attacker and Attacker Spawner
    Unfinished: Lose Trigger
    Next: Add animation event

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+    [Range(0f, 5f)]
+    [SerializeField] float moveSpeed = 1f;
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.left * moveSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    // config
+    [SerializeField] Attacker attackerPrefab;
+
+    // state var
+    bool spawning = true;
+    float maxSpawnGap = 5f;
+    float minSpawnGap = 1f;
+
+    // for view
+    [SerializeField] float spawnGap;
+
+    IEnumerator Start()
+    {
+        while (spawning)
+        {
+            spawnGap = UnityEngine.Random.Range(minSpawnGap, maxSpawnGap);
+            yield return new WaitForSeconds(spawnGap);
+            SpawnAttacker();
+        }
+    }
+
+    private void SpawnAttacker()
+    {
+        Instantiate(attackerPrefab, transform.position, transform.rotation);
+    }
+
+    void Update()
+    {
+    }
+}
