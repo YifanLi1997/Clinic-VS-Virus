3ed5ce450fa90a6c21e659383872b13b2f9f0818
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2020-02-15 22:44:15 +0800

    Added VFX & SFX to attacker

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Attacker : MonoBehaviour
 {
+    [Header("Movement")]
     [Range(0f, 5f)]
     [SerializeField] float currentSpeed = 1f;
+
+    [Header("Health")]
     [SerializeField] int health = 500;
+    [SerializeField] GameObject deathVFXPrefab;
+    [SerializeField] AudioClip deathSFX;
     
     void Start()
     {
@@ -28,8 +33,10 @@ public class Attacker : MonoBehaviour
         health -= damage;
         if (health <= 0)
         {
-            // play death animation
+            var deathVFX = Instantiate(deathVFXPrefab, transform.position, transform.rotation);
+            AudioSource.PlayClipAtPoint(deathSFX, transform.position);
             Destroy(gameObject);
+            Destroy(deathVFX, 1f);
         }
     }
 }
