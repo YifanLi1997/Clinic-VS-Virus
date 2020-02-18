7640be520ad8e85aadbe69f6daf4a8de9b832b16
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2020-02-19 01:35:08 +0800

    defender only attack while attackers exist in its lane

--- Assets/Scripts/Defender/Shooter.cs
+++ Assets/Scripts/Defender/Shooter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +7,57 @@ public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] GameObject gunPosition;
+
+    Animator m_animator;
+    AttackerSpawner m_attackerSpawner;
+
+    private void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_attackerSpawner = FindMySpawner();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            // shoot
+            m_animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            // idle
+            m_animator.SetBool("isAttacking", false);
+        }
+        
+    }
+
+    private AttackerSpawner FindMySpawner()
+    {
+        var spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (var spawner in spawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(transform.position.y - spawner.transform.position.y) <= Mathf.Epsilon);
+
+            if (isCloseEnough)
+            {
+                 return spawner;
+            }
+        }
+
+        return null;
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (m_attackerSpawner.transform.childCount > 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
 
     public void Fire()
     {
